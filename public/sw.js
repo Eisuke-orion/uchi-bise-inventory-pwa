8c20ca2941cccaa8a1b975a8c67e0567b871c651
EISUKE <eisuke@EISUKEnoMacBook-Air.local>
2026-07-25 01:08:58 +0900

    Add pineapple juice to inventory

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'uchi-bise-inventory-v5';
+const CACHE = 'uchi-bise-inventory-v6';
 const BASE = self.registration.scope;
 const asset = path => new URL(path, BASE).href;
 const APP_SHELL = [BASE, asset('index.html'), asset('manifest.json'), asset('icons/icon-192.png'), asset('icons/icon-512.png')];
